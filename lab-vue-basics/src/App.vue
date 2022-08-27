60bc80a750f199dbc723f4c306ce80d9cc53f044
grumpycat711 <eugeni.puigdomenech@gmail.com>
2022-08-27 15:44:32 +0200

    Iteration1

---
+++ lab-vue-basics/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <navbar />
+  <div>aqui la app</div>
+  <footerC />
+</template>
+
+<script>
+import footerC from "./components/footerComp.vue";
+import navbar from "./components/navBarComp.vue";
+
+export default {
+  components: {
+    footerC,
+    navbar,
+  },
+};
+</script>
+
+<style></style>
---
+++ lab-vue-basics/src/components/footerComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>soy footer, la operacion es {{ operacion }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      operacion: 2 + 2,
+    };
+  },
+  computed: {
+    propertyComputed() {
+      return operacion;
+    },
+  },
+};
+
+</script>
+
+<style></style>
