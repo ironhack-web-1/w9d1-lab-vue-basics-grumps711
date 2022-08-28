d97f7adeee60cc05c8f5853771c29cbf1eccee9c
grumpycat711 <eugeni.puigdomenech@gmail.com>
2022-08-28 12:15:22 +0200

    Iteration 4 done

--- lab-vue-basics/src/components/navBarComp.vue
+++ lab-vue-basics/src/components/navBarComp.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>aqui navbar y el resultado de esta operacion es {{ operacionDelNavBar }}</div>
+  <div>
+    aqui navbar y el resultado de esta operacion es {{ operacionDelNavBar }}
+  </div>
+  <div>
+  
+   <h3>Titles</h3>
+    <ul>
+      <li v-for="eso in Items" :key="eso.title">
+        {{ eso.title }}
+      </li>
+    </ul>
+
+     <h3>Descriptions</h3>
+    <ul>
+      <li v-for="eso in Items" :key="eso.description">
+        {{ eso.description }}
+      </li>
+    </ul>
+
+     <h3>Content</h3>
+    <ul>
+      <li v-for="eso in Items" :key="eso.content">
+        {{ eso.content }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -7,13 +32,27 @@ export default {
   data() {
     return {
       operation: 3 * 6,
+      Items: [
+        { title: "Hola" ,
+         description: "Brutal" ,
+         content: "acción" },
+      
+        { title: "Pelicula" ,
+         description: "Increible" ,
+         content: "Terror" },
+      
+        { title: "Buena" ,
+         description: "Espectacular" ,
+         content: "Romance" },
+      ],
     };
   },
-  computed:{
-    operacionDelNavBar(){
+
+  computed: {
+    operacionDelNavBar() {
       return this.operation;
-    }
-  }
+    },
+  },
 };
 </script>
 
